f11921f2cc35862802c1f843ee0b377fb0f9178f
Akira-TL <2273076505@qq.com>
2025-03-01 13:39:46 +0800

    基础图像处理方式

--- __init__.py
+++ __init__.py
@@ -7,3 +7,14 @@
 @作者    :Akira_TL
 @版本    :1.0
 """
+
+from PIL import Image
+import numpy as np
+
+if __name__ == "__main__":
+    # 读取图片
+    background = Image.open("figs/test1.png")
+    former = Image.open("figs/former.png")
+    former = former.resize((background.size[0], background.size[1]))
+    background.paste(former, (0, 0), former)
+    background.save("figs/test_out.png")
